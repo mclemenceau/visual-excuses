8e50b37bfd9cce73f60c6c309982a642e5ba26d4
Matthieu Clemenceau <matthieu.clemenceau@canonical.com>
2025-03-23 18:15:12 -0500

    reordered arguments and fixed typos

--- visual_excuses/excuses_parser.py
+++ visual_excuses/excuses_parser.py
@@ -7,27 +7,15 @@ class ExcusesParser:
             description="Ubuntu Excuses (Proposed Migration) Viewer")
 
         self.parser.add_argument(
-            "--ftbfs",
-            action="store_true",
-            help="Show only FTBFS packages"
-        )
-
-        self.parser.add_argument(
-            "--json",
-            action="store_true",
-            help="Output in JSON format"
-        )
-
-        self.parser.add_argument(
-            "--limit",
-            type=int,
-            help="Limit the number of results shown"
-        )
-
-        self.parser.add_argument(
             "--name",
             type=str,
             help="Regex to filter package names (case-insensitive)"
+        )
+
+        self.parser.add_argument(
+            "--ftbfs",
+            action="store_true",
+            help="Show only FTBFS packages"
         )
 
         self.parser.add_argument(
@@ -40,6 +28,18 @@ class ExcusesParser:
             "--max-age",
             type=int,
             help="Only include packages no older than this many days"
+        )
+
+        self.parser.add_argument(
+            "--limit",
+            type=int,
+            help="Limit the number of results shown"
+        )
+
+        self.parser.add_argument(
+            "--json",
+            action="store_true",
+            help="Output in JSON format"
         )
 
     def parse_args(self, argv=None):
