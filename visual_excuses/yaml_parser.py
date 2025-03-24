0516d607fcf36f227551f2a01f801423fd62aa58
Matthieu Clemenceau <matthieu.clemenceau@canonical.com>
2025-03-23 21:12:28 -0500

    when component is empty it is in main

--- visual_excuses/yaml_parser.py
+++ visual_excuses/yaml_parser.py
@@ -35,7 +35,7 @@ def load_excuses(file_path: str) -> List[Excuse]:
         excuses_list.append(
             Excuse(
                 item_name=entry.get("item-name", ""),
-                component=entry.get("component", ""),
+                component=entry.get("component", "main"),
                 new_version=str(entry.get("new-version", "")),
                 missing_builds=(
                     entry.get("missing-builds", {}).get("on-architectures", [])
