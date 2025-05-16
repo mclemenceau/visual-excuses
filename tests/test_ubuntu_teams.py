a7ed92e6aa3107c9c1f56a233da7710151a8b672
Dave Jones <dave.jones@canonical.com>
2025-05-16 09:59:54 +0200

    Fix up the tests
    Still not comprehensive but good enough for now

--- tests/test_ubuntu_teams.py
+++ tests/test_ubuntu_teams.py
@@ -1,5 +1,8 @@
-import pytest
+import io
+import json
 from unittest.mock import patch, Mock
+
+import pytest
 
 from visual_excuses.ubuntu_teams import UbuntuTeamMapping
 
@@ -8,6 +11,7 @@ FAKE_MAPPING = {
     "team-b": ["gnome", "dbus", "ptyxis"],
     "team-c": ["kernel"]
 }
+FAKE_MAPPING_RAW = json.dumps(FAKE_MAPPING).encode('utf-8')
 
 
 @pytest.fixture
@@ -16,36 +20,39 @@ def mock_requests_get():
         yield mock_get
 
 
-def test_init_succes_with_mapping(mock_requests_get):
+def test_init_succes_with_mapping(tmp_path, mock_requests_get):
 
     mock_response = Mock()
     mock_response.status_code = 200
-    mock_response.json.return_value = FAKE_MAPPING
+    mock_response.headers = {'ETag': 'foo'}
+    mock_response.raw = io.BytesIO(FAKE_MAPPING_RAW)
     mock_requests_get.return_value = mock_response
 
-    team_mapping = UbuntuTeamMapping()
+    team_mapping = UbuntuTeamMapping(cache_dir=tmp_path)
 
     assert (team_mapping.mapping == FAKE_MAPPING)
 
 
-def test_init_failure_without_connection(mock_requests_get):
+def test_init_failure_without_connection(tmp_path, mock_requests_get):
 
     mock_response = Mock()
     mock_response.status_code = 404
     mock_requests_get.return_value = mock_response
 
-    with pytest.raises(RuntimeError, match="Failed to load Ubuntu team "):
-        UbuntuTeamMapping()
+    with pytest.raises(ValueError, match="Unexpected HTTP response "):
+        UbuntuTeamMapping(cache_dir=tmp_path)
 
 
-def test_get_teams_returns_correct_teams_for_package(mock_requests_get):
+def test_get_teams_returns_correct_teams_for_package(
+    tmp_path, mock_requests_get):
 
     mock_response = Mock()
     mock_response.status_code = 200
-    mock_response.json.return_value = FAKE_MAPPING
+    mock_response.headers = {'ETag': 'foo'}
+    mock_response.raw = io.BytesIO(FAKE_MAPPING_RAW)
     mock_requests_get.return_value = mock_response
 
-    team_mapping = UbuntuTeamMapping()
+    team_mapping = UbuntuTeamMapping(cache_dir=tmp_path)
 
     assert team_mapping.get_teams("bash") == ["team-a"]
     assert team_mapping.get_teams("gnome") == ["team-b"]
@@ -53,39 +60,43 @@ def test_get_teams_returns_correct_teams_for_package(mock_requests_get):
 
 
 def test_get_teams_returns_empty_list_teams_for_unknown_package(
-        mock_requests_get):
+        tmp_path, mock_requests_get):
 
     mock_response = Mock()
     mock_response.status_code = 200
-    mock_response.json.return_value = FAKE_MAPPING
+    mock_response.headers = {'ETag': 'foo'}
+    mock_response.raw = io.BytesIO(FAKE_MAPPING_RAW)
     mock_requests_get.return_value = mock_response
 
-    team_mapping = UbuntuTeamMapping()
+    team_mapping = UbuntuTeamMapping(cache_dir=tmp_path)
 
     assert team_mapping.get_teams("vim") == []
 
 
-def test_default_team_returns_first_team(mock_requests_get):
+def test_default_team_returns_first_team(tmp_path, mock_requests_get):
 
     mock_response = Mock()
     mock_response.status_code = 200
-    mock_response.json.return_value = FAKE_MAPPING
+    mock_response.headers = {'ETag': 'foo'}
+    mock_response.raw = io.BytesIO(FAKE_MAPPING_RAW)
     mock_requests_get.return_value = mock_response
 
-    team_mapping = UbuntuTeamMapping()
+    team_mapping = UbuntuTeamMapping(cache_dir=tmp_path)
 
     assert team_mapping.default_team("bash") == "team-a"
     assert team_mapping.default_team("gnome") == "team-b"
     assert team_mapping.default_team("dbus") == "team-a"
 
 
-def test_default_team_returns_empty_string_if_no_teams(mock_requests_get):
+def test_default_team_returns_empty_string_if_no_teams(
+    tmp_path, mock_requests_get):
 
     mock_response = Mock()
     mock_response.status_code = 200
-    mock_response.json.return_value = FAKE_MAPPING
+    mock_response.headers = {'ETag': 'foo'}
+    mock_response.raw = io.BytesIO(FAKE_MAPPING_RAW)
     mock_requests_get.return_value = mock_response
 
-    team_mapping = UbuntuTeamMapping()
+    team_mapping = UbuntuTeamMapping(cache_dir=tmp_path)
 
     assert team_mapping.default_team("vim") == ""
